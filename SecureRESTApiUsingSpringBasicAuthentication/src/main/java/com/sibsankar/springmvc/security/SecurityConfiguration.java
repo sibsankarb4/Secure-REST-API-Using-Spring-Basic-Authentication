0cb6e04ec5ecf53cf76031afe3495164c5fda281
Sibsankar Bera <sibsankarb4@gmail.com>
2019-03-08 11:43:48 +0530

    Update SecurityConfiguration.java

--- SecureRESTApiUsingSpringBasicAuthentication/src/main/java/com/sibsankar/springmvc/security/SecurityConfiguration.java
+++ SecureRESTApiUsingSpringBasicAuthentication/src/main/java/com/sibsankar/springmvc/security/SecurityConfiguration.java
@@ -18,8 +18,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("bill").password("abc123").roles("ADMIN");
-		auth.inMemoryAuthentication().withUser("tom").password("abc123").roles("USER");
+		auth.inMemoryAuthentication().withUser("sibsankar").password("xyz123").roles("ADMIN");
+		auth.inMemoryAuthentication().withUser("sbera").password("xyz123").roles("USER");
 	}
 	
 	@Override
