4477e6db921fa2d00c552f25ac85a97acd371be7
Sibsankar Bera <sibsankarb4@gmail.com>
2019-03-08 11:37:27 +0530

    Add files via upload

---
+++ SecureRESTApiUsingSpringBasicAuthentication/src/main/java/com/sibsankar/springmvc/service/UserService.java
@@ -0,0 +1,27 @@
+package com.sibsankar.springmvc.service;
+
+import java.util.List;
+
+import com.sibsankar.springmvc.model.User;
+
+
+
+public interface UserService {
+	
+	User findById(long id);
+	
+	User findByName(String name);
+	
+	void saveUser(User user);
+	
+	void updateUser(User user);
+	
+	void deleteUserById(long id);
+
+	List<User> findAllUsers(); 
+	
+	void deleteAllUsers();
+	
+	public boolean isUserExist(User user);
+	
+}
